e8523fc3281b564fd97eb1c18d8068a85db49f87
litomax01 <litomax01@hotmail.com>
2025-01-31 21:29:09 -0500

    final

--- evaluacion/App.tsx
+++ evaluacion/App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box, styles.purple, styles.leftBox]} />
+      <View style={[styles.box, styles.orange, styles.centerBox]} />
+      <View style={[styles.box, styles.cyan, styles.rightBox]} />
     </View>
   );
 }
@@ -13,8 +16,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#1D3141',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: width * 0.18,
+    height: width * 0.18,
+    borderWidth: 3,
+    borderColor: 'white',
+    position: 'absolute',
+  },
+  purple: {
+    backgroundColor: '#6A5ACD',
+  },
+  orange: {
+    backgroundColor: '#F4A460',
+  },
+  cyan: {
+    backgroundColor: '#00CED1',
+  },
+  leftBox: {
+    top: '48%',
+    left: '32%',
+    transform: [{ translateY: -width * 0.09 }],
+  },
+  centerBox: {
+    top: '50%',
+    left: '60%',
+    transform: [{ translateX: -width * 0.09 }],
+  },
+  rightBox: {
+    top: '48%',
+    left: '70%',
+    transform: [{ translateY: -width * 0.09 }],
   },
 });
